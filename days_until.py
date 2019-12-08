31c1366571482ccf9a5805aed8f3178e3de0cee8
Aaron Lichtman <aaronlichtman@gmail.com>
2019-12-08 01:55:26 +0100

    Basic functionality somewhat working

--- days_until.py
+++ days_until.py
@@ -1,7 +1,11 @@
-import yaml
-from datetime import date
-from colorama import Fore, Style
+# Standard Library Imports
+import sys
+from datetime import date, datetime
 from os import environ, path
+
+# 3rd-Party Imports
+import yaml
+from colorama import Fore, Style
 
 
 ########
@@ -14,18 +18,23 @@ def get_config_path():
     return path.join(xdg_config, "days_until.yaml")
 
 
-def create_config_if_nonexistent(path):
+def create_config_if_nonexistent(config_path):
     """Creates the config file if it doesn't exist"""
-    if not path.exists(path):
-        with open(path, 'w'):
+    if not path.exists(config_path):
+        with open(config_path, 'w'):
             pass
-        print_notification(f"Created config at: {path}")
+        print_notification(f"Created config at: {config_path}")
 
 
 def read_config(config_path):
     """Reads the YAML config and returns it as a dict"""
     with open(config_path, "r") as f:
-        data = yaml.full_load(f)
+        try:
+            data = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print_error("Config was unparsable. Make sure it is properly formatted.")
+            print(exc)
+            sys.exit()
     return data
 
 
@@ -56,21 +65,35 @@ def print_notification(msg):
     print(GREEN + BOLD + f"{msg}")
 
 
-def print_progress_chart(start_date, end_date):
+#################
+# Per-Event Logic
+#################
+
+def show_progress_chart(start_date, end_date):
     """Displays the progress chart for an entry"""
+    today = date.today()
     total_days = calculate_days_between(start_date, end_date)
-    days_past_start = calculate_days_between(start_date, date.today())
-    percentage_complete = days_past_start // total_days
+    days_past_start = calculate_days_between(start_date, today)
+    days_remaining = total_days - days_past_start
+    percentage_complete = round((days_past_start / total_days) * 100, 1)
+    print(GREEN + BOLD + f"Current Date: {today}")
+    print(GREEN + BOLD + f"Days Remaining: {days_remaining}")
+    
+    fill_char = "▓"
+    empty_char = "▓"
     # TODO: Print the chart with pretty graphics
-    print(f"{percentage_complete}%")
+    print(f"{percentage_complete}%\n")
 
-
-############
-# Date Logic
-############
 
 def calculate_days_between(start_date, target_date):
-    return (start_date - target_date).days
+    return (target_date - start_date).days
+
+
+def show_entry(entry_data):
+    print_section_header(entry_data["event"], BLUE)
+    start_date = datetime.strptime(entry_data["dates"]["start"], "%Y-%m-%d").date()
+    end_date = datetime.strptime(entry_data["dates"]["end"], "%Y-%m-%d").date()
+    show_progress_chart(start_date, end_date)
 
 
 ######
@@ -83,11 +106,13 @@ def main():
     config_path = get_config_path()
     create_config_if_nonexistent(config_path)
     data = read_config(config_path)
-    if not data:
-        print_error("No data in config.")
+    print(data)
+    if data is None:
+        print_error(f"No data in config: {config_path}")
+        sys.exit()
 
-    for entry in data:
-        print(entry)
+    for key in data:
+        show_entry(data[key])
 
 
 if __name__ == "__main__":
